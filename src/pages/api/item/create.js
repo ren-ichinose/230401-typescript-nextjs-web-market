08d52cba2227eb2b68a069ae7fb34151e63636ad
Ren Ichinose <kamatani.toru@gmail.com>
2023-04-03 19:56:01 +0900

    backend_create機能の実装 #2

--- src/pages/api/item/create.js
+++ src/pages/api/item/create.js
@@ -1,9 +1,14 @@
 import connectDB from 'utils/database';
+import { ItemModel } from 'utils/schemaModels';
 
-const createItem = (req, res) => {
-  connectDB();
-  console.log(req.body.title);
-  return res.status(200).json({ message: 'アイテムを作成しました' });
+const createItem = async (req, res) => {
+  try {
+    await connectDB();
+    await ItemModel.create(req.body);
+    return res.status(200).json({ message: 'アイテムを作成しました' });
+  } catch (error) {
+    return res.status(400).json({ message: 'アイテムを作成できませんでした' });
+  }
 };
 
-export default createItem; 
+export default createItem;
