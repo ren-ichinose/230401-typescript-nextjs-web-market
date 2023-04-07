a7e8a5e2eade6eb45068eeb977b54540bb48581d
Ren Ichinose <kamatani.toru@gmail.com>
2023-04-07 20:33:15 +0900

    backend_schema_UserModelの実装 #6

--- utils/schemaModels.js
+++ utils/schemaModels.js
@@ -10,4 +10,14 @@ const ItemSchema = new Schema({
   email: String,
 });
 
-export const ItemModel = mongoose.models.Item || mongoose.model('Item', ItemSchema);
+const UserScema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+export const ItemModel =
+  mongoose.models.Item || mongoose.model('Item', ItemSchema);
+
+export const UserModel =
+  mongoose.models.User || mongoose.model('User', UserScema);
