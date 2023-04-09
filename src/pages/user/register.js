3b5fa2186b8b03fac2e26efad1631740c14bff83
Ren Ichinose <kamatani.toru@gmail.com>
2023-04-09 22:43:53 +0900

    frontend_ユーザー登録画面の実装 #12

--- src/pages/user/register.js
+++ src/pages/user/register.js
@@ -1,9 +1,69 @@
+import { useState } from 'react';
+
 const Register = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/user/register`,
+        {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ name, email, password }),
+        }
+      );
+      const { message } = await res.json();
+      alert(message);
+    } catch (error) {
+      alert('ユーザー登録に失敗しました');
+    }
+  };
+
   return (
     <div>
       <h1>ユーザー登録</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="name"
+          required
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          name="email"
+          placeholder="email"
+          requireds 
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          name="password"
+          placeholder="password"
+          required
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button>登録</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
