b650da5c74db70696ac77de2d3cf8418be36236b
Ren Ichinose <kamatani.toru@gmail.com>
2023-04-10 10:47:03 +0900

    frontend_getAllItem_getServerSidePropsの実装 #13

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
-export default function Home() {
-  return ( 
-    <div>Hello World</div>
-  )
-}
+const Home = ({ message, allItems }) => {
+  return <div>Home</div>;
+};
+
+export default Home;
+
+export const getServerSideProps = async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/item/readall`
+  );
+  const allItems = await res.json();
+  return {
+    props: allItems,
+  };
+};
