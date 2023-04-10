62e091d9ca7fdfa5ac2ff5ad1d81c92ba494a875
Ren Ichinose <kamatani.toru@gmail.com>
2023-04-10 09:40:47 +0900

    frontend_ログイン画面の実装 #12

--- src/pages/user/login.js
+++ src/pages/user/login.js
@@ -1,7 +1,58 @@
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+import { useState } from 'react';
 
-export default Login
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/user/login`,
+        {
+          method: 'post',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email, password }),
+        }
+      );
+      const { message, accessToken } = await res.json();
+      localStorage.setItem('accesToken', accessToken);
+    } catch (error) {
+      alert('ログインができませんでした');
+    }
+  };
+
+  return (
+    <div>
+      <h1>ログイン</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="email"
+          placeholder="email"
+          required
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          name="password"
+          placeholder="password"
+          required
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button>ログイン</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
